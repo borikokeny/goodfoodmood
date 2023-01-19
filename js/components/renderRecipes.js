f4f9aae02ec0676596f14bec70cc25436ae167ac
borikokeny <95857961+borikokeny@users.noreply.github.com>
2023-01-19 17:52:16 +0100

    search update

--- js/components/renderRecipes.js
+++ js/components/renderRecipes.js
@@ -2,30 +2,53 @@ export  function renderRecipes(recipeToRender){
   const recipeContainer = document.querySelector(".results");
   recipeContainer.innerHTML = "";
 
-  recipeToRender.forEach(function(searchResults) {
-      recipeContainer.innerHTML += `<div class="result">
-                                   <h4>${searchResults.title} 
-                                   </h4>
-                                   <div class="image" style="background-image:       
-                                    url(${searchResults.image})"></div>
-                                   <h5></h5>
-                                   </div>`;
-  });
-  // for(let i = 0; i < recipeToRender.length; i++) {
+  // recipeToRender.forEach(function(searchResults) {
+  //     recipeContainer.innerHTML += `<div class="result">
+  //                                  <h4>${searchResults.title} 
+  //                                  </h4>
+  //                                  <div class="image" style="background-image:       
+  //                                   url(${searchResults.image})"></div>
+  //                                  <h5></h5>
+  //                                  </div>`;
+  // });
 
-  //   if(recipeToRender[i].vegetarian)
-  //   // console.log(recipeToRender[i].title);
+  
+  for(let i = 0; i < recipeToRender.length; i++) {
 
-  //     // let recipes = recipeToRender[i];
+    // if(recipeToRender[i].vegetarian)
+    // console.log(recipeToRender[i].title);
 
-  //   recipeContainer.innerHTML += `<div class="result">
-  //                                 <h4>${recipeToRender[i].title} 
-  //                                 </h4>
-  //                                 <div class="image" style="background-image:       
-  //                                 url(${recipeToRender[i].image})" alt="${recipeToRender[i].title}"></div>
-  //                                 <h5></h5>
-  //                                 </div>`;
-  // }
+      // let recipes = recipeToRender[i];
+
+    const szavak = recipeToRender[i].diets;
+  
+
+    let osszeg = "";
+
+    for (let i = 0; i < szavak.length; i++) {
+      osszeg = osszeg + `<h5>${szavak[i]}</h5>` ;
+    }
+    // console.log(osszeg);
+
+    const ujSzavak = recipeToRender[i].extendedIngredients;
+
+    let ujOsszeg = "";
+
+    for (let i = 0; i < ujSzavak.length; i++) {
+      ujOsszeg = ujOsszeg + `<h6>${ujSzavak[i].name}</h6>`;
+    }
+
+    // console.log(ujOsszeg);
+
+    recipeContainer.innerHTML += `<div class="result">
+                                  <h4>${recipeToRender[i].title} 
+                                  </h4>
+                                  <div class="image" style="background-image:       
+                                  url(${recipeToRender[i].image})" alt="${recipeToRender[i].title}"></div>
+                                  ${osszeg}
+                                  ${ujOsszeg}
+                                  </div>`;
+  }
 }
 
 // for(let i = 0; i < recipeToRender.length; i++) {
--- js/components/searchRecipes.js
+++ js/components/searchRecipes.js
@@ -5,17 +5,36 @@ import { renderRecipes } from "./renderRecipes.js";
 export function searchRecipes(recipes) {
 
   const search = document.querySelector(".search");
- 
+  
   search.onkeyup = function(event) {
-      
+   
   event.preventDefault();
-   
+
    const searchValue = event.target.value.trim().toLowerCase();
+
+ 
    
-   const filteredRecipes = recipes.filter(function (searchResults) {
-    if (searchResults.title.toLowerCase().includes(searchValue)) {
-      return true;            
+   const filteredRecipes = recipes.filter(function (recipeToRender) {
+    if (recipeToRender.title.toLowerCase().includes(searchValue)) {
+
+      return true;       
     }
+
+    for (let i = 0; i < recipeToRender.diets.length; i++) {
+      if (recipeToRender.diets[i].toLowerCase().includes(searchValue)) {
+        return true;
+      }
+    }
+
+    for (let i = 0; i < recipeToRender.extendedIngredients.length; i++) {
+      if (recipeToRender.extendedIngredients[i].name.toLowerCase().includes(searchValue)) {
+        return true;
+      }
+    }
+    
+
+//  console.log(recipeToRender.diets); 
+// console.log(recipeToRender.occasions); ez mar mukszik
 
     // const ingredients = searchResults.extendedIngredients.name;
 
@@ -31,9 +50,8 @@ export function searchRecipes(recipes) {
     //   return true;
     // }
 
-
    });
-
+    console.log(filteredRecipes);
    renderRecipes(filteredRecipes);
    };
 }
